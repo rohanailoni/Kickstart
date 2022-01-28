a74628c5219c879efe842baeec52a59c7de58de1
rohan aileni <rohanailoni@gmail.com>
2022-01-28 12:23:16 +0530

    Practice for kickstart Template

---
+++ 2021/round1/Goodness.java
@@ -0,0 +1,10 @@
+
+import java.io.*;
+public class Goodness{
+
+        public static void main(String args[]){
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            
+        }
+
+}
